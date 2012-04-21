ef0aa36ee132371f3f077d4a3157a715f7310eee
Nicholas Bourke <nicholas.bourke@gmail.com>
2012-04-22 09:19:04 +1000

    initial commit

---
+++ MediaSorter.Tests/BaseTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MediaSorter.Tests
+{
+    public class BaseTest
+    {
+        public BaseTest()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+    }
+}
---
+++ MediaSorter.Tests/FilenameParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MediaSorter.Tests
+{
+    [TestClass]
+    public class FilenameParser : BaseTest
+    {
+        public class ParseSeason : FilenameParser
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
